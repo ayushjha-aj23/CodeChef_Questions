2d246947b4aafe84d95505fbb3af7e2ced163722
Ayush Jha <ayushjha.aj23@gmail.com>
2022-07-05 22:51:54 +0530

    Create Level_Up_BPC_01_RichieRich.java

---
+++ Level_Up_BPC_01_RichieRich.java
@@ -0,0 +1,32 @@
+// Richie Rich Problem Code:CHFRICH
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int test_cases = sc.nextInt();
+		
+		while(test_cases>0)
+		{
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    int X = sc.nextInt();
+		    
+		    int no_years = (B-A)/X;
+		    
+		    System.out.println(no_years);
+		    test_cases--;
+		}
+	}
+}
