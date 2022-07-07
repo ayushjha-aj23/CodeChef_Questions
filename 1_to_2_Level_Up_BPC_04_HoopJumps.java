7e137f93eb30a2b80b7a00aa5654386531a90cd2
Ayush Jha <ayushjha.aj23@gmail.com>
2022-07-07 19:49:01 +0530

    Add files via upload

---
+++ 1_to_2_Level_Up_BPC_04_HoopJumps.java
@@ -0,0 +1,28 @@
+// Hoop Jump Problem Code: HOOPS
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int test_cases = sc.nextInt();
+		
+		while(test_cases>0)
+		{
+		    int N = sc.nextInt();
+		    
+		    System.out.println((N+1)/2);
+		    test_cases--;
+		}
+	}
+}
