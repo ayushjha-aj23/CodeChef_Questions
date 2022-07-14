3716a3682c2b8088a40d228bf26b1542a0cdd8a9
Ayush Jha <ayushjha.aj23@gmail.com>
2022-07-14 19:22:13 +0530

    Add files via upload

---
+++ 02_LevelUp_1_to_2_Stars/01_BPC/05_ChefAndSpells.java
@@ -0,0 +1,32 @@
+// Chef And Spells Problem Code: CHFSPL
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int test_cases = sc.nextInt();
+		
+		while(test_cases-- > 0)
+		{
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    int C = sc.nextInt();
+		    
+		    int X = (int) Math.min(A, Math.min(B,C) );
+		    //System.out.println("Min deduction"+X);
+		    
+		    System.out.println(A+B+C - X);
+		}
+	}
+}
