9b9275c15d77584c83a33cdda55a4dc84757926e
Ayush Jha <ayushjha.aj23@gmail.com>
2022-12-27 15:30:40 +0530

    Add files via upload

---
+++ 02_LevelUp_1_to_2_Stars/02_TimeComplexity/12_POLIN.java
@@ -0,0 +1,32 @@
+/* package codechef; don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            
+            HashSet<Integer> S1 = new HashSet<>();
+            HashSet<Integer> S2 = new HashSet<>();
+            
+            for(int i=0;i<n;i++)
+            {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                
+                S1.add(x);
+                S2.add(y);
+            }
+            System.out.println(S1.size() + S2.size());
+        }
+    }
+}
